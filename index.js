d76e07b5538d3a2ee4f1b238b8cea81668ca1c14
riley soloner <rileysoloner@gmail.com>
2022-06-16 13:42:16 -0400

    commit tree f2c052da793d6d7ad425589cd678511aad46b6c8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Riley is the champion"
+
+document.body.append(newHeader);
